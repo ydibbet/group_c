8b91915db90e2af4be236f57ee174dea09fd10e6
Youri <yjm.dibbet@student.han.nl>
2024-11-25 15:33:26 +0100

    [Hypertune] bugfix

--- hypertune/hypertune.py
+++ hypertune/hypertune.py
@@ -3,6 +3,7 @@ from typing import Dict
 from mads_hackathon import datasets, metrics
 from mads_datasets.base import BaseDatastreamer
 from mltrainer.preprocessors import BasePreprocessor
+from mads_hackathon.models import CNNConfig as Config
 import ray
 import torch
 import mlflow
@@ -28,23 +29,32 @@ def train(config: Dict):
     ray will modify the values inside the config before it is passed to the train
     function.
     """
-
+    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = torch.device("mps")
+    else:
+        device = "cpu"  # type: ignore
+    logger.info(f"Using {device}")
+    if device != "cpu":
+        logger.warning(
+            f"using acceleration with {device}." "Check if it actually speeds up!"
+        )
+        
     data_dir = config["data_dir"]
     # gesturesdatasetfactory = DatasetFactoryProvider.create_factory(DatasetType.GESTURES)
     preprocessor = BasePreprocessor()
     trainfile = (data_dir / "heart_big_train.parq").resolve()
     validfile = (data_dir / "heart_big_valid.parq").resolve()
     trainfile.exists(), validfile.exists()
-
+    
+    traindataset = datasets.HeartDataset2D(trainfile, target="target", shape=config["matrixshape"])
+    validdataset = datasets.HeartDataset2D(validfile, target="target", shape=config["matrixshape"])
+    traindataset.to(device)
+    validdataset.to(device)
+        
     with FileLock(data_dir / ".lock"):
         # we lock the datadir to avoid parallel instances trying to
-        traindataset = datasets.HeartDataset2D(trainfile, target="target", shape=config["matrixshape"])
-        validdataset = datasets.HeartDataset2D(validfile, target="target", shape=config["matrixshape"])
-        traindataset.to(device)
-        validdataset.to(device)
-        
-        trainstreamer = BaseDatastreamer(traindataset, preprocessor = preprocessor, batchsize=config.batchsize)
-        validstreamer = BaseDatastreamer(validdataset, preprocessor = preprocessor, batchsize=config.batchsize)
+        trainstreamer = BaseDatastreamer(traindataset, preprocessor = preprocessor, batchsize=config["batchsize"])
+        validstreamer = BaseDatastreamer(validdataset, preprocessor = preprocessor, batchsize=config["batchsize"])
 
     # we set up the metric
     # and create the model with the config
@@ -70,27 +80,17 @@ def train(config: Dict):
     # This is why we set earlystop_kwargs=None, because we
     # are handing over this control to ray.
 
-    if torch.backends.mps.is_available() and torch.backends.mps.is_built():
-        device = torch.device("mps")
-    else:
-        device = "cpu"  # type: ignore
-    logger.info(f"Using {device}")
-    if device != "cpu":
-        logger.warning(
-            f"using acceleration with {device}." "Check if it actually speeds up!"
-        )
-
     trainer = Trainer(
         model=model,
         settings=trainersettings,
         loss_fn=torch.nn.CrossEntropyLoss(),
-        optimizer=torch.optim.Adam,  # type: ignore
+        optimizer=torch.optim.Adam, # type: ignore
         traindataloader=trainstreamer.stream(),
         validdataloader=validstreamer.stream(),
         scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau,
         device=device,
-    )
-
+        )
+    
     # modify the tags when you change them!
     mlflow.set_tag("model", "CNN")
     mlflow.set_tag("dataset", "heart2D")
@@ -98,38 +98,24 @@ def train(config: Dict):
     mlflow.log_param("scheduler", "None")
     mlflow.log_param("earlystop", "None")
 
-    mlflow.log_params(asdict(config))
+    mlflow.log_params(config)
     mlflow.log_param("epochs", trainersettings.epochs)
-    mlflow.log_param("matrix0", config.matrixshape[0])
-    mlflow.log_param("matrix1", config.matrixshape[1])
+    mlflow.log_param("matrix0", config["matrixshape"][0])
+    mlflow.log_param("matrix1", config["matrixshape"][1])
     mlflow.log_param("optimizer", str(trainer.optimizer))
     mlflow.log_params(trainersettings.optimizer_kwargs)
-
-    trainer = Trainer(
-        model=model,
-        settings=trainersettings,
-        loss_fn=trainersettings.loss_fn,
-        optimizer=trainersettings.optimizer,
-        traindataloader=trainstreamer.stream(),
-        validdataloader=validstreamer.stream(),
-        scheduler=None,
-        device=device,
-        )
     
     trainer.loop()
     cfm = caluclate_cfm(model, validstreamer)
     for i, tp in enumerate(np.diag(cfm)):
         mlflow.log_metric(f"TP_{i}", tp)
-
+        
 if __name__ == "__main__":
     NUM_SAMPLES = 10
     MAX_EPOCHS = 10
     ray.init()
 
-    data_dir = Path("../hackathon-data/").resolve()
-    if not data_dir.exists():
-        data_dir.mkdir(parents=True)
-        logger.info(f"Created {data_dir}")
+    data_dir = Path("/home/ydibbet/mads-hackathon/hackathon-data").resolve()
     tune_dir = Path("models/ray").resolve()
     search = HyperOptSearch()
     scheduler = AsyncHyperBandScheduler(
@@ -140,21 +126,22 @@ if __name__ == "__main__":
     )
 
     config = {
-        "batchsize": 64,
-        "input_size": 1,
+        "epochs": 1,
         "output_size": 20,
         "tune_dir": tune_dir,
         "data_dir": data_dir,
-        "hidden_size": tune.randint(16, 128),
-        "kernel_size": tune.randint(1, 12),
-        "maxpool": tune.randint(1, 4),
         "dropout": tune.uniform(0.0, 0.3),
-        "num_layers": tune.randint(2, 5),
-        "num_classes": tune.randint(2, 5),
+        "dev": "youri",
         "matrixshape": (4,48),
-        "dev": "youri"
+        "batchsize": 64,
+        "input_channels": 1,
+        "hidden": 16, # tune.randint(16, 128),
+        "kernel_size": 3, # tune.randint(1, 12),
+        "maxpool": 2, # tune.randint(1, 4),
+        "num_layers": 1, # tune.randint(2, 5),
+        "num_classes": 5, # tune.randint(2, 5),  
     }
-
+    
     reporter = CLIReporter()
     reporter.add_metric_column("Accuracy")
 
